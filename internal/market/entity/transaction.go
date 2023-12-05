f80cdeb8bb6761a0d5af796ec3093b662e05f71d
goular7 <gustavogoulart1970@gmail.com.br>
2023-12-04 23:44:09 -0300

    endDay1

--- internal/market/entity/transaction.go
+++ internal/market/entity/transaction.go
@@ -1,7 +1,9 @@
 package entity
 
-import ("time"
-		"github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
 )
 
 type Transaction struct {
@@ -25,4 +27,28 @@ func NewTransaction(SellingOrder *Order, buyingOrder *Order, shares int, price f
 		Total: 			total,
 		DateTime:		time.Now(),
 	}
+}
+
+func (t *Transaction) CalculateTotal(shares int, price float64) {
+	t.Total = float64(t.Shares) * t.Price
+}
+
+func (t *Transaction) CloseBuyOrder() {
+	if t.BuyingOrder.PendingShares == 0 {
+		t.BuyingOrder.Status = "CLOSED"
+	}
+}
+
+func (t *Transaction) CloseSellOrder() {
+	if t.SellingOrder.PendingShares == 0 {
+		t.SellingOrder.Status = "CLOSED"
+	}
+}
+
+func (t* Transaction) AddBuyOrderPadingShares(shares int) {
+	t.BuyingOrder.PendingShares += shares
+}
+
+func (t* Transaction) AddSellOrderPadingShares(shares int) {
+	t.SellingOrder.PendingShares += shares
 }
